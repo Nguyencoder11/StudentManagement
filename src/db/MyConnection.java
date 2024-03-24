312c4cd72cdea0ed1a571b22b7398b7daa835c15
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-03-24 21:50:38 +0700

    first commit

---
+++ src/db/MyConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+import java.sql.Connection;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class MyConnection {
+
+    private static final String username = "Leviz";
+    private static final String password = "Levizz#311";
+    private static final String dataConn = "jdbc:mysql://localhost:3306/students_management";
+    private static Connection con = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(dataConn, username, password);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+}
---
+++ src/student/Student.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import db.MyConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Student {
+    
+    Connection con = MyConnection.getConnection();
+    PreparedStatement ps;
+
+    // Get table max row
+    public int getMax() {
+        int id = 0;
+        Statement st;
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery("select max(id) from student");
+            while (rs.next()) {
+                id = rs.getInt(1);
+            }
+        } catch (Exception e) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return id + 1;
+    }
+
+    // Insert data into student table
+    public void insert(int id, String sname, String date, String gender, String email, String phone, String father, String mother, String address1, String address2, String image_path) {
+        String sql = "insert into student values(?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.setString(2, sname);
+            ps.setString(3, date);
+            ps.setString(4, gender);
+            ps.setString(5, email);
+            ps.setString(6, phone);
+            ps.setString(7, father);
+            ps.setString(8, mother);
+            ps.setString(9, address1);
+            ps.setString(10, address2);
+            ps.setString(11, image_path);
+            
+            if (ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "New student added successfully!");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // Check student email address is already exists
+    public boolean isEmailExist(String email) {
+        try {
+            ps = con.prepareStatement("select * from student where email = ?");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    // Check student phone number is already exists
+    public boolean isPhoneExist(String phone) {
+        try {
+            ps = con.prepareStatement("select * from student where phone = ?");
+            ps.setString(1, phone);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    // get all the students value from database table
+    public void getStudentValue(JTable table, String searchValue) {
+        String sql = "select * from student where concat(name,email,phone) like ? order by id desc";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, "%" + searchValue + "%");
+            ResultSet rs = ps.executeQuery();
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            Object[] row;
+            while (rs.next()) {
+                row = new Object[11];
+                row[0] = rs.getInt(1);
+                row[1] = rs.getString(2);
+                row[2] = rs.getString(3);
+                row[3] = rs.getString(4);
+                row[4] = rs.getString(5);
+                row[5] = rs.getString(6);
+                row[6] = rs.getString(7);
+                row[7] = rs.getString(8);
+                row[8] = rs.getString(9);
+                row[9] = rs.getString(10);
+                row[10] = rs.getString(11);
+                model.addRow(row);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
